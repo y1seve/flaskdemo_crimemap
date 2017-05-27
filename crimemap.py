f20b7dcc0f930a9a18665e0690d2646eb70800a3
y1seve <yushuai09@gmail.com>
2017-05-27 19:49:11 +0800

    validating the category

--- crimemap.py
+++ crimemap.py
@@ -11,6 +11,8 @@ from flask import request
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 
 @app.route('/')
 def home():
@@ -22,7 +24,7 @@ def home():
     # return render_template('home.html', data=data)
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template('home.html', crimes = crimes)
+    return render_template('home.html', crimes = crimes, categories = categories)
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -45,6 +47,8 @@ def clear():
 @app.route('/submitcrime', methods=['POST'])
 def submitcrime():
     category = request.form.get('category')
+    if category not in categories:
+        return home()
     date = request.form.get('date')
     latitude = float(request.form.get('latitude'))
     longitude = float(request.form.get('longitude'))
